2507e25e05616190be45aad0ca8b4f6550ae3923
y579 <84591776+y579@users.noreply.github.com>
2021-06-23 14:58:37 -0700

    Update HealthIcon.hx
    ya

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -30,7 +30,7 @@ class HealthIcon extends FlxSprite
 		animation.add('senpai', [22, 22], 0, false, isPlayer);
 		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
 		animation.add('spirit', [23, 23], 0, false, isPlayer);
-		animation.add('bf-old', [14, 15], 0, false, isPlayer);
+		//why the FUCK does old bf still exist???
 		animation.add('gf', [16], 0, false, isPlayer);
 		animation.add('gf-christmas', [16], 0, false, isPlayer);
 		animation.add('gf-pixel', [16], 0, false, isPlayer);
